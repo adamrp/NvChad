f0d1d6d8a8abfa159c8fb71102abd0acad6e9e56
Adam Robbins-Pianka <adam.robbins-pianka@benchling.com>
2023-06-22 23:26:00 -0600

    slight tweaks

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,18 +1,19 @@
 local plugins = {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+        "lua-language-server",
+      },
+    },
+  },
+  {
     "jose-elias-alvarez/null-ls.nvim",
     ft = {"python"},
     opts = function()
       return require "custom.configs.null-ls"
     end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "pyright",
-      },
-    },
   },
   {
     "neovim/nvim-lspconfig",
