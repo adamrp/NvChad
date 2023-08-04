7d7965c6599c3c6e1151f6d8707fe6940fa7cdf0
Adam Robbins-Pianka <adam.robbins-pianka@benchling.com>
2023-08-03 20:35:26 -0600

    Brighten Comment nd LineNr colors

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,14 @@
 require "custom.vim_opts"
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin', transparency = true}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = {
+  theme = 'catppuccin',
+  transparency = true,
+  hl_override = {
+     LineNr = {fg = "light_grey"},
+     Comment = {fg = "teal"},
+  },
+}
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
