74b098f4a71200b68e7e7d265d0381ea5d7445cb
Adam Robbins-Pianka <adamrp@gmail.com>
2024-01-30 22:54:46 -0700

    Correct editorconfig configs

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -4,25 +4,27 @@ vim.env.PATH = string.gsub(vim.env.PATH, p, "")
 
 local autocmd = vim.api.nvim_create_autocmd
 
+require('editorconfig').properties.colorcolumn = function(bufnr, val)
+  vim.opt.colorcolumn = val
+end
+
 autocmd("FileType", {
   pattern = "python",
-  callback = function()
+  callback = function(opts)
     vim.opt.colorcolumn = "110"
+    require('editorconfig').config(opts.buf)
   end
 })
 
 autocmd("FileType", {
   pattern = {"lua", "json", "yaml", "gitconfig", "toml"},
-  callback = function()
+  callback = function(opts)
     vim.opt.tabstop = 2
     vim.opt.softtabstop = 2
     vim.opt.shiftwidth = 2
     vim.opt.expandtab = true
+    require('editorconfig').config(opts.buf)
   end
 })
-
-require('editorconfig').properties.colorcolumn = function(bufnr, val)
-  vim.opt.colorcolumn = val
-end
 
 require "custom.vim_opts"
